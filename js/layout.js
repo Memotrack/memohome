a3121619ee89b266602914242a2bd8c5c187f389
Guna <gs400pass@gmail.com>
2026-05-04 02:16:49 +0530

    init

---
+++ js/layout.js
@@ -0,0 +1,12 @@
+async function loadComponent(id, file) {
+
+const response = await fetch(file);
+
+const data = await response.text();
+
+document.getElementById(id).innerHTML = data;
+
+}
+
+loadComponent("header", "/components/header.html");
+loadComponent("footer", "/components/footer.html");
